f594a1ffec705d42bfcc6563264c47f98a2b9291
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2023-03-11 12:25:56 +0300

    Extentions - UICollectionView+Ext.swift

--- Hisnul Muslim/Extentions/UICollectionView.swift
+++ Hisnul Muslim/Extentions/UICollectionView.swift
@@ -1,8 +1,17 @@
 //
-//  UICollectionView.swift
+//  UICollectionView+Ext.swift
 //  Hisnul Muslim
 //
 //  Created by Abdirizak Hassan on 3/10/23.
 //
 
 import Foundation
+import UIKit
+
+extension UICollectionView {
+    
+    func reloadDataOnMainThread() {
+        DispatchQueue.main.async { self.reloadData() }
+    }
+    
+}

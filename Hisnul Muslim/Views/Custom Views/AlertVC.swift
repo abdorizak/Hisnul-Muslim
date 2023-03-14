c0d5945be57358d0485c558ac83bee8c1d7db55d
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2023-03-14 19:34:23 +0300

    Views - Custom AlertVC

---
+++ Hisnul Muslim/Views/Custom Views/AlertVC.swift
@@ -0,0 +1,101 @@
+//
+//  AlertVC.swift
+//  Hisnul Muslim
+//
+//  Created by Abdirizak Hassan on 3/14/23.
+//
+
+import Foundation
+import UIKit
+
+class AlertVC: UIViewController {
+    
+    let containerView   = AlertContainerView()
+    let titleLabel      = Label(textAlignment: .center, 20)
+    let messageLabel    = Label(textAlignment: .center)
+    let actionButton    = Button(color: .systemPink, title: "Ok", systemImageName: "checkmark.circle")
+    
+    var alertTitle: String?
+    var message: String?
+    var buttonTitle: String?
+    
+    let padding: CGFloat = 20
+    
+    
+    init(title: String, message: String, buttonTitle: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.alertTitle     = title
+        self.message        = message
+        self.buttonTitle    = buttonTitle
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
+        view.addSubViews(containerView, titleLabel, actionButton, messageLabel)
+        
+        configureContainerView()
+        configureTitleLabel()
+        configureActionButton()
+        configureMessageLabel()
+    }
+    
+    
+    func configureContainerView() {
+        NSLayoutConstraint.activate([
+            containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerView.widthAnchor.constraint(equalToConstant: 280),
+            containerView.heightAnchor.constraint(equalToConstant: 220)
+        ])
+    }
+    
+    
+    func configureTitleLabel() {
+        titleLabel.text = alertTitle ?? "Something went wrong"
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: padding),
+            titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
+            titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+            titleLabel.heightAnchor.constraint(equalToConstant: 28)
+        ])
+    }
+    
+    
+    func configureActionButton() {
+        actionButton.setTitle(buttonTitle ?? "Ok", for: .normal)
+        actionButton.addTarget(self, action: #selector(dismissVC), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            actionButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+    
+    
+    func configureMessageLabel() {
+        messageLabel.text           = message ?? "Unable to complete request"
+        messageLabel.numberOfLines  = 4
+        
+        NSLayoutConstraint.activate([
+            messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            messageLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
+            messageLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+            messageLabel.bottomAnchor.constraint(equalTo: actionButton.topAnchor, constant: -12)
+        ])
+    }
+    
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+}

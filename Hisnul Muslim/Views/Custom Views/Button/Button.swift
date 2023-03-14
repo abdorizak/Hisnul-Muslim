4cc8d31d8dce9bd6f970c9b7e6bac25b803495cb
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2023-03-14 19:34:39 +0300

    Views - Custom Button

---
+++ Hisnul Muslim/Views/Custom Views/Button/Button.swift
@@ -0,0 +1,46 @@
+//
+//  Button.swift
+//  Hisnul Muslim
+//
+//  Created by Abdirizak Hassan on 3/14/23.
+//
+
+import Foundation
+import UIKit
+
+class Button: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    convenience init(color: UIColor, title: String, systemImageName: String) {
+        self.init(frame: .zero)
+        set(color: color, title: title, systemImageName: systemImageName)
+    }
+    
+    
+    private func configure() {
+        configuration = .tinted()
+        configuration?.cornerStyle = .medium
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
+    final func set(color: UIColor, title: String, systemImageName: String) {
+        configuration?.baseBackgroundColor = color
+        configuration?.baseForegroundColor = color
+        configuration?.title = title
+        
+        configuration?.image = UIImage(systemName: systemImageName)
+        configuration?.imagePadding = 6
+        configuration?.imagePlacement = .leading
+    }
+}

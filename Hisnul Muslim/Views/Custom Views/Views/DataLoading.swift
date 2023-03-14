a7d28198b1a2769f1378d6d2a13e2aababfcac07
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2023-03-14 19:37:34 +0300

    Views

---
+++ Hisnul Muslim/Views/Custom Views/Views/DataLoading.swift
@@ -0,0 +1,51 @@
+//
+//  DataLoading.swift
+//  Hisnul Muslim
+//
+//  Created by Abdirizak Hassan on 3/14/23.
+//
+
+import Foundation
+import UIKit
+
+class HSDataLoadingVC: UIViewController {
+    
+    var containerView: UIView!
+
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor   = .systemBackground
+        containerView.alpha             = 0
+        
+        UIView.animate(withDuration: 0.25) { self.containerView.alpha = 0.8 }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: containerView.centerXAnchor)
+        ])
+        
+        activityIndicator.startAnimating()
+    }
+    
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            self.containerView.removeFromSuperview()
+            self.containerView = nil
+        }
+    }
+    
+    
+    func showEmptyStateView(with message: String, in view: UIView) {
+        let emptyStateView = EmptyStateView(message: message)
+        emptyStateView.frame = view.bounds
+        view.addSubview(emptyStateView)
+    }
+}
---
+++ Hisnul Muslim/Views/Custom Views/Views/EmptyStateView.swift
@@ -0,0 +1,69 @@
+//
+//  EmptyStateView.swift
+//  Hisnul Muslim
+//
+//  Created by Abdirizak Hassan on 3/14/23.
+//
+
+import Foundation
+import UIKit
+
+class EmptyStateView: UIView {
+    
+    let messageLabel = Label(textAlignment: .center, 28)
+    let logoImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+   
+    
+    init(message: String) {
+        super.init(frame: .zero)
+        messageLabel.text = message
+        configure()
+    }
+    
+    
+    
+    private func configure() {
+        addSubViews(messageLabel, logoImageView)
+        configureMessageLabel()
+        configureLogoImageView()
+    }
+    
+    private func configureMessageLabel() {
+        messageLabel.numberOfLines = 3
+        messageLabel.textColor = .secondaryLabel
+        let labelCenterYConstraint: CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? -80 : -150
+        
+        NSLayoutConstraint.activate([
+            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: labelCenterYConstraint),
+            messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+            messageLabel.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    private func configureLogoImageView() {
+        logoImageView.image = Images.emptyState
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.layer.opacity = 0.1
+        let logoBottomConstant: CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? 80 : 40
+        
+        NSLayoutConstraint.activate([
+            logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
+            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: logoBottomConstant)
+            
+        ])
+    }
+
+}
+

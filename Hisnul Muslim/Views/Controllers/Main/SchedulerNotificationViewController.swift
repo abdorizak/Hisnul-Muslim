e6fe0f128d551ebf0cb59b3f762ca3e8ebdf711a
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2023-03-19 14:34:58 +0300

    Schduler Notification VC

--- Hisnul Muslim/Views/Controllers/Main/SchedulerNotificationViewController.swift
+++ Hisnul Muslim/Views/Controllers/Main/SchedulerNotificationViewController.swift
@@ -9,10 +9,11 @@ import UIKit
 
 class SchedulerNotificationViewController: UIViewController {
 
+    var content: Content!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
     }
     
 

8fbfea77686f4594ce5c4ef3f8d2953767504f2d
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2023-03-19 14:32:06 +0300

    SchedulerNotificationViewController.swift

---
+++ Hisnul Muslim/Views/Controllers/Main/SchedulerNotificationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SchedulerNotificationViewController.swift
+//  Hisnul Muslim
+//
+//  Created by Abdirizak Hassan on 3/19/23.
+//
+
+import UIKit
+
+class SchedulerNotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

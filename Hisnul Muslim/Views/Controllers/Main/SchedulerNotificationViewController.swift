b943f15327e03e00d6eab3d352a87cf0618788bf
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2023-03-20 08:25:35 +0300

    Added -timePicker and Notify Button

--- Hisnul Muslim/Views/Controllers/Main/SchedulerNotificationViewController.swift
+++ Hisnul Muslim/Views/Controllers/Main/SchedulerNotificationViewController.swift
@@ -10,21 +10,41 @@ import UIKit
 class SchedulerNotificationViewController: UIViewController {
 
     var content: Content!
+    private lazy var timePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        picker.datePickerMode = .time
+        picker.preferredDatePickerStyle = .wheels
+        picker.locale = .current
+        picker.translatesAutoresizingMaskIntoConstraints = false
+        picker.timeZone = .autoupdatingCurrent
+        picker.backgroundColor = .systemBackground
+        return picker
+    }()
+    private lazy var notifyMeBtn = Button(color: .systemRed, title: "Notify Me", systemImageName: "bell.badge")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        configSchedulerNotificationVC()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configSchedulerNotificationVC() {
+        view.backgroundColor = .systemBackground
+        view.addSubViews(timePicker, notifyMeBtn)
+        configTimePickerAndButton()
     }
-    */
-
+    
+    private func configTimePickerAndButton() {
+        
+        NSLayoutConstraint.activate([
+            timePicker.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            timePicker.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            timePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
+            notifyMeBtn.topAnchor.constraint(equalTo: timePicker.bottomAnchor, constant: 30),
+            notifyMeBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            notifyMeBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            notifyMeBtn.heightAnchor.constraint(equalToConstant: 55)
+        ])
+    }
+    
 }

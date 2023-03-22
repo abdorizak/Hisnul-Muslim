dca9d57f91daebfde8be20eef95eae7c5e0ec033
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2023-03-22 21:02:09 +0300

    added Core Data lazy var persistentContainer

--- Hisnul Muslim/Resources/AppDelegate.swift
+++ Hisnul Muslim/Resources/AppDelegate.swift
@@ -7,10 +7,20 @@
 
 import UIKit
 import UserNotifications
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "MyDataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -41,9 +51,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-            let token = deviceToken.reduce("") { $0 + String(format: "%02x", $1) }
-            print(token)
-        }
+        let token = deviceToken.reduce("") { $0 + String(format: "%02x", $1) }
+        print(token)
+    }
     
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         print(error)

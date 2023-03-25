0b1165415750b31ee38f2400d0dbab8fbc99859a
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2023-03-25 10:28:34 +0300

    View Model: HSMSchedulersViewModel

--- Hisnul Muslim/View Model/HSMSchedulersViewModel.swift
+++ Hisnul Muslim/View Model/HSMSchedulersViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+protocol SchedulerDelegate: AnyObject {
+    func didFinishLoadingSchedulers()
+}
+
+final class HSMSchedulerViewModel {
+    let schedulerdata = HSMCoreDataHelper()
+    private(set) var schedulers = [HSMSchedulers]()
+    weak var delegate: SchedulerDelegate!
+    init() { }
+    
+    func index(_ index: Int) -> HSMSchedulers {
+        self.schedulers[index]
+    }
+    
+    func fetchSchedulersData() {
+        let schedulers_result = schedulerdata.fetchData()
+        guard let result = schedulers_result else { fatalError("N/A") }
+        self.schedulers = result
+        self.delegate.didFinishLoadingSchedulers()
+    }
+}

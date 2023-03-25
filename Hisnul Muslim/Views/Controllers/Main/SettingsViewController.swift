2b8ca1c440b3deed8f974c347629c585cc259772
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2023-03-25 10:32:09 +0300

    App Settings View Controller

--- Hisnul Muslim/Views/Controllers/Main/SettingsViewController.swift
+++ Hisnul Muslim/Views/Controllers/Main/SettingsViewController.swift
@@ -7,24 +7,96 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private struct Sections {
+        let options: [SettingOptions]
+    }
+    private struct SettingOptions {
+        let icon: UIImage?
+        let title: String
+        let handler: () -> Void?
 
+    }
+    private var models: [Sections] = []
+    private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureSVC()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        configTableHeaderView()
+    }
+    
+    private func configureSVC() {
         view.backgroundColor = .systemBackground
         title = "اختيارات"
         navigationController?.navigationBar.prefersLargeTitles = true
+        tableView = UITableView(frame: view.bounds, style: .insetGrouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.identifier)
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        configTableView()
+        configTableHeaderView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configTableView() {
+        models.append(Sections(options: [
+            SettingOptions(icon: Images.timeScheduled, title: "إشعارات", handler: {
+                // Go to the Schedulers Notifications
+                let schedulersVC = SchedulersListVC()
+                schedulersVC.modalTransitionStyle = .coverVertical
+                schedulersVC.modalPresentationStyle = .pageSheet
+                self.present(schedulersVC, animated: true)
+                
+            }),
+            SettingOptions(icon: Images.bookInfo, title: "معلومات الكتاب", handler: {
+                //
+            })
+        ]))
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
-    */
-
+    
+    private func configTableHeaderView() {
+        let headerView = SettingTableHeaderView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.width, height: 250))
+        tableView.tableHeaderView = headerView
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        models.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        models[section].options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.section].options[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.identifier, for: indexPath) as! SettingsTableViewCell
+        cell.displayData(title: model.title, Image: model.icon)
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let model = models[indexPath.section].options[indexPath.row]
+        model.handler()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        45
+    }
+    
 }

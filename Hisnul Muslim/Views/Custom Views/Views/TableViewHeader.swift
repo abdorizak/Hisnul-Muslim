2b11f75d860e1c4a7fd357d47d03dc84cee58210
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2023-03-25 10:36:19 +0300

    Custom Table Header View

--- Hisnul Muslim/Views/Custom Views/Views/TableViewHeader.swift
+++ Hisnul Muslim/Views/Custom Views/Views/TableViewHeader.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import UIKit
+
+class SettingTableHeaderView: UIView {
+    
+    let imageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configHeader()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        configHeader()
+    }
+    
+    private func configHeader(){
+        // Add the image view as a subview and set its constraints
+        addSubview(imageView)
+        imageView.image = UIImage(named: "AppIcon")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 20
+        imageView.layer.shadowColor = UIColor.label.cgColor
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: topAnchor, constant: 20),
+            imageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

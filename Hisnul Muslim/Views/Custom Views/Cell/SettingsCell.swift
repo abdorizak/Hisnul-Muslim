889445f6dfd56141b29059ee4bc75eb75b061f47
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2023-03-25 10:33:42 +0300

    Custom Settings Cell

--- Hisnul Muslim/Views/Custom Views/Cell/SettingsCell.swift
+++ Hisnul Muslim/Views/Custom Views/Cell/SettingsCell.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+    static let identifier = String(describing: SettingsTableViewCell.self)
+    private var title = Label(textAlignment: .right, 15)
+    private var img   = UIImageView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        cellConfig()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        cellConfig()
+    }
+    
+    private func cellConfig() {
+        contentView.addSubViews(img, title)
+        title.textAlignment = .right
+        img.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            // align top and bottom of content view with cell's contentView
+            contentView.topAnchor.constraint(equalTo: title.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: title.bottomAnchor),
+            
+            // constraints for image view and title label
+            img.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            img.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            img.heightAnchor.constraint(equalToConstant: 30),
+            img.widthAnchor.constraint(equalToConstant: 30),
+            title.trailingAnchor.constraint(equalTo: img.leadingAnchor, constant: -8),
+            title.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+        
+    }
+    
+    func displayData(title: String, Image: UIImage?) {
+        self.title.text = title
+        self.img.image = Image
+    }
+}
+

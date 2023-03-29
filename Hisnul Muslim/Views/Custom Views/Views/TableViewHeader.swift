15f9889ffa5d7d253d6d899f5ca815bdeefdbc3a
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2023-03-29 13:03:31 +0300

    changed the image name

--- Hisnul Muslim/Views/Custom Views/Views/TableViewHeader.swift
+++ Hisnul Muslim/Views/Custom Views/Views/TableViewHeader.swift
@@ -17,6 +17,10 @@ class SettingTableHeaderView: UIView {
         configHeader()
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         configHeader()
@@ -25,21 +29,14 @@ class SettingTableHeaderView: UIView {
     private func configHeader(){
         // Add the image view as a subview and set its constraints
         addSubview(imageView)
-        imageView.image = UIImage(named: "AppIcon")
+        imageView.image = UIImage(named: "HisnulMuslimBook")
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.clipsToBounds = true
-        imageView.layer.cornerRadius = 20
-        imageView.layer.shadowColor = UIColor.label.cgColor
         NSLayoutConstraint.activate([
             imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
             imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
             imageView.topAnchor.constraint(equalTo: topAnchor, constant: 20),
             imageView.heightAnchor.constraint(equalToConstant: 200)
         ])
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }

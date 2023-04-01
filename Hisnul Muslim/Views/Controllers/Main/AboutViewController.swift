ce789e0f93210fc00f629053f419c67b8b9be3ea
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2023-04-01 08:24:51 +0300

    about app VC

---
+++ Hisnul Muslim/Views/Controllers/Main/AboutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AboutViewController.swift
+//  Hisnul Muslim
+//
+//  Created by Abdirizak Hassan on 4/1/23.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

7d1b0333c81ec917cda808876f5f68c413479d1a
abdorizak <cabdirizaaqyare12@gmail.com>
2024-07-07 10:39:25 +0300

    refactor: Update HSMViewModel, HSMSchedulerViewModel, DetailsViewModel, AdkarFavoritesListViewModel, HSMSchedulerModel, PresentencyManager, and HSMCoreDataHelper to use Combine and async/await for improved event handling, state management, fetching, deleting, retrieving, inserting, and updating records

--- Hisnul Muslim/Views/Controllers/Main/AdkarFavoritesListVC.swift
+++ Hisnul Muslim/Views/Controllers/Main/AdkarFavoritesListVC.swift
@@ -6,35 +6,48 @@
 //
 
 import UIKit
+import Combine
 
-class AdkarFavoritesListVC: HSDataLoadingVC {
+class AdkarFavoritesListVC: UIViewController {
     
-    let tableView               = UITableView()
-    var favorites: [Content]    = []
     
+    var vm = AdkarFavoritesListViewModel()
+    private var cancellables = Set<AnyCancellable>()
+    private lazy var dataSource = DataSource(tableView: tableView) { [weak self] tableView, indexPath, row in
+        switch row {
+        case let .noFavorites(image, text, secondaryText):
+            let cell = tableView.dequeueReusableCell(withIdentifier: EmptyStateViewCell.identifier, for: indexPath) as! EmptyStateViewCell
+            cell.displayData(title: text, message: secondaryText, image: image, iconSize: CGSize(width: 50, height: 50))
+            return cell
+        case let .favorite(content):
+            let cell = tableView.dequeueReusableCell(withIdentifier: AdkarFavoriteCell.identifier, for: indexPath) as! AdkarFavoriteCell
+            cell.displayData(list: content)
+            return cell
+        }
+    }
+    
+    
+    private lazy var tableView = {
+        $0.delegate = self
+        $0.separatorStyle = .none
+        $0.keyboardDismissMode = .onDrag
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        
+        $0.register(EmptyStateViewCell.self)
+        $0.register(AdkarFavoriteCell.self)
+        return $0
+    }(UITableView())
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
         configureTableView()
+        bindViewModel()
     }
-    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        getFavorites()
-    }
-    
-    override func updateContentUnavailableConfiguration(using state: UIContentUnavailableConfigurationState) {
-        if favorites.isEmpty {
-            var config = UIContentUnavailableConfiguration.empty()
-            config.image = UIImage(systemName: "bookmark.fill")
-            config.text = "لا يوجد هنا أي مفضلات"
-            config.secondaryText = "ليس لديك أي أدعية مفضلة، أضف اذهب وقم بتفعيل الأدعية المفضلة لديك"
-            contentUnavailableConfiguration = config
-        } else {
-            contentUnavailableConfiguration = nil
-        }
+        vm.getFavorites()
     }
     
     
@@ -48,84 +61,115 @@ class AdkarFavoritesListVC: HSDataLoadingVC {
     func configureTableView() {
         view.addSubview(tableView)
         
-        tableView.frame         = view.bounds
-        tableView.rowHeight     = 80
-        tableView.delegate      = self
-        tableView.dataSource    = self
-        tableView.removeExcessCells()
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    private func bindViewModel() {
+        vm.event
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] in self?.handleEvent($0) }
+            .store(in: &cancellables)
         
-        tableView.register(AdkarFavoriteCell.self, forCellReuseIdentifier: AdkarFavoriteCell.identifier)
+        vm.$state
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] in self?.handleState($0) }
+            .store(in: &cancellables)
     }
     
+    private func handleEvent(_ event: AdkarFavoritesListViewModel.Event) {
+        switch event {
+        case .error(let error):
+            self.presentAlertOnMainThread(title: "Error", message: error.localizedDescription)
+        case .didRemoveFavorite:
+            self.presentAlertOnMainThread(title: "تم الحذف", message: "تم حذف الدعاء من المفضلة بنجاح")
+        }
+    }
     
-    func getFavorites() {
-        PersistenceManager.retrieveFavorites { [weak self] result in
-            guard let self = self else { return }
-            
-            switch result {
-            case .success(let favorites):
-                self.updateUI(with: favorites)
-            case .failure(let error):
-                DispatchQueue.main.async {
-                    self.presentAlert(title: "هناك خطأ ما", message: error.rawValue, buttonTitle: "Ok")
-                }
+    private func handleState(_ state: AdkarFavoritesListViewModel.State) {
+        var snapshot = Snapshot()
+        snapshot.appendSections([.main])
+        
+        if state.favorites.isEmpty {
+            if !state.isLoading {
+                snapshot.appendItems([.noFavorites(UIImage(systemName: "bookmark.fill") ?? .hisnulMuslimBook, "لا يوجد هنا أي مفضلات", "ليس لديك أي أدعية مفضلة، أضف اذهب وقم بتفعيل الأدعية المفضلة لديك")], toSection: .main)
             }
+        } else {
+            snapshot.appendItems(state.favorites.map { .favorite($0) }, toSection: .main)
         }
-    }
-    
-    func updateUI(with favorites: [Content]) {
-        self.favorites = favorites
-        setNeedsUpdateContentUnavailableConfiguration()
-        DispatchQueue.main.async {
-            self.tableView.reloadData()
-            self.view.bringSubviewToFront(self.tableView)
-        }
+        
+        dataSource.applyOnMainThread(snapshot, animatingDifferences: true)
     }
     
 }
 
 
-extension AdkarFavoritesListVC: UITableViewDataSource, UITableViewDelegate {
+extension AdkarFavoritesListVC {
+    typealias DataSource = UITableViewDiffableDataSource<Section, Row>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Row>
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return favorites.count
+    enum Section: CaseIterable {
+        case main
     }
     
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: AdkarFavoriteCell.identifier) as! AdkarFavoriteCell
-        let favorite = favorites[indexPath.row]
-        cell.displayData(list: favorite)
-        return cell
+    enum Row: Hashable {
+        
+        case noFavorites(UIImage, String, String), favorite(Content)
+        
+        static func == (lhs: Row, rhs: Row) -> Bool {
+            return lhs.hashValue == rhs.hashValue
+        }
+        
+        func hash(into hasher: inout Hasher) {
+            switch self {
+            case let .noFavorites(image, text, secondaryText):
+                hasher.combine(values: "noFavorites", image, text, secondaryText)
+            case let .favorite(content):
+                hasher.combine(values: "favorite", content)
+            }
+        }
+        
     }
-    
+}
+
+extension AdkarFavoritesListVC: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedObj = favorites[indexPath.item]
-        let detailVC = DetailsViewController()
-        detailVC.content = selectedObj
-        let navigationController = UINavigationController(rootViewController: detailVC)
-        navigationController.modalPresentationStyle = .fullScreen
-        navigationController.modalTransitionStyle = .coverVertical
-        present(navigationController, animated: true, completion: nil)
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        guard let row = dataSource.itemIdentifier(for: indexPath) else { return }
+        
+        switch row {
+        case .noFavorites:
+            break
+        case .favorite(let content):
+            let detailVC = DetailsViewController()
+            detailVC.vm = .init(content: content)
+            let navigationController = UINavigationController(rootViewController: detailVC)
+            navigationController.modalPresentationStyle = .fullScreen
+            navigationController.modalTransitionStyle = .coverVertical
+            present(navigationController, animated: true, completion: nil)
+        }
+        
     }
     
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        guard editingStyle == .delete else { return }
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        guard let row = dataSource.itemIdentifier(for: indexPath) else { return nil }
         
-        PersistenceManager.updateWith(favorite: favorites[indexPath.row], actionType: .remove) { [weak self] error in
-            guard let self = self else { return }
-            guard let error = error else {
-                self.favorites.remove(at: indexPath.row)
-                tableView.deleteRows(at: [indexPath], with: .left)
-                setNeedsUpdateContentUnavailableConfiguration()
-                return
+        switch row {
+        case .favorite(let content):
+            let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { [weak self] _, _, completionHandler in
+                self?.vm.removeFavorite(favorite: content)
+                completionHandler(true)
             }
-            
-            DispatchQueue.main.async {
-                self.presentAlert(title: "غير قادر على الإزالة", message: error.rawValue, buttonTitle: "Ok")
-            }
+            deleteAction.backgroundColor = .systemRed
+            return UISwipeActionsConfiguration(actions: [deleteAction])
+        default:
+            return nil
         }
     }
 }

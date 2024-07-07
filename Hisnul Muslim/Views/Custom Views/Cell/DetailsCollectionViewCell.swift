cb647c47bf581171a9cb5552326b994aaa46367b
abdorizak <cabdirizaaqyare12@gmail.com>
2024-07-07 10:44:21 +0300

    refactor: Remove unused detailView in DetailsCollectionViewCell

--- Hisnul Muslim/Views/Custom Views/Cell/DetailsCollectionViewCell.swift
+++ Hisnul Muslim/Views/Custom Views/Cell/DetailsCollectionViewCell.swift
@@ -10,12 +10,6 @@ import UIKit
 class DetailsCollectionViewCell: UICollectionViewCell {
     static let identifier = String(describing: DetailsViewController.self)
     
-    private let detailView: UIView = {
-        let detail_view = UIView(frame: .zero)
-        detail_view.translatesAutoresizingMaskIntoConstraints = false
-        return detail_view
-    }()
-    
     private let dua_details: UITextView = {
         let textView = UITextView(frame: .zero)
         textView.font = .systemFont(ofSize: 23)
@@ -39,15 +33,12 @@ class DetailsCollectionViewCell: UICollectionViewCell {
     
     private func configDetailsViewCell() {
         contentView.addSubview(dua_details)
-        dua_details.backgroundColor = .tertiarySystemBackground
+        dua_details.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
         NSLayoutConstraint.activate([
-            dua_details.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 10),
+            dua_details.topAnchor.constraint(equalTo: contentView.topAnchor),
             dua_details.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
             dua_details.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             dua_details.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-            
-            
-            
         ])
     }
     

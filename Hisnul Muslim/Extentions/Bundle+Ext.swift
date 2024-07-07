d2f5bfda3fbf5a9145a6eea0aef4ce50139446e5
abdorizak <cabdirizaaqyare12@gmail.com>
2024-07-07 10:22:19 +0300

    refactor: Update Bundle extension to use Combine for file reading

--- Hisnul Muslim/Extentions/Bundle+Ext.swift
+++ Hisnul Muslim/Extentions/Bundle+Ext.swift
@@ -5,27 +5,25 @@
 //  Created by Abdirizak Hassan on 2/17/23.
 //
 
-import Foundation
 import UIKit
+import Combine
 
 extension Bundle {
-    func decode<T: Decodable>(_ type: T.Type, from file: String) -> T {
-        guard let url = self.url(forResource: file, withExtension: nil) else {
-            fatalError("Failed to locate \(file) in bundle.")
+    func readFile(_ file: String) -> AnyPublisher<Data, HSErrors> {
+        guard let fileURL = self.url(forResource: file, withExtension: nil) else {
+            return Fail(error: HSErrors.fileNotFound).eraseToAnyPublisher()
         }
         
-        guard let data = try? Data(contentsOf: url) else {
-            fatalError("Failed to load \(file) from bundle.")
-        }
-        
-        let decoder = JSONDecoder()
-        
-        do {
-            let loaded = try decoder.decode(T.self, from: data)
-            return loaded
-        } catch {
-            print(error)
-            fatalError("Failed to decode \(error) from bundle.")
-        }
+        return Just(fileURL)
+            .tryMap { try Data(contentsOf: $0) }
+            .mapError { _ in HSErrors.unableToReadFile }
+            .eraseToAnyPublisher()
+    }
+    
+    func decodable<T: Decodable>(type Model: T.Type, _ fileName: String) -> AnyPublisher<T, HSErrors> {
+        readFile(fileName)
+            .decode(type: T.self, decoder: JSONDecoder())
+            .mapError { _ in HSErrors.unableToReadFile }
+            .eraseToAnyPublisher()
     }
 }

37653611347b58e7a5d7e7d676b7380d7416c41a
abdorizak <cabdirizaaqyare12@gmail.com>
2024-07-07 10:42:47 +0300

    refactor: Update SettingsViewController to use UITableViewDiffableDataSource for improved data handling and display

--- Hisnul Muslim/Views/Controllers/Main/SettingsViewController.swift
+++ Hisnul Muslim/Views/Controllers/Main/SettingsViewController.swift
@@ -7,22 +7,28 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SettingsViewController: UIViewController {
     
-    private struct Sections {
-        let options: [SettingOptions]
+    private lazy var dataSource = DataSource(tableView: tableView) { [weak self] tableView, indexPath, row in
+        switch row {
+            
+        case .notification:
+            let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.identifier, for: indexPath) as! SettingsTableViewCell
+            cell.displayData(title: "إشعارات", Image: Images.timeScheduled)
+            return cell
+            
+        case .about:
+            let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.identifier, for: indexPath) as! SettingsTableViewCell
+            cell.displayData(title: "معلومات الكتاب", Image: Images.bookInfo)
+            return cell
+            
+        }
     }
-    private struct SettingOptions {
-        let icon: UIImage?
-        let title: String
-        let handler: () -> Void?
-
-    }
-    private var models: [Sections] = []
-    private let tableView: UITableView = {
+    
+    private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .insetGrouped)
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.identifier)
+        tableView.register(SettingsTableViewCell.self)
         return tableView
     }()
     
@@ -41,66 +47,107 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         view.backgroundColor = .systemBackground
         title = "اختيارات"
         navigationController?.navigationBar.prefersLargeTitles = true
-        view.addSubview(tableView)
-        tableView.dataSource = self
         tableView.delegate = self
         configTableView()
         configTableHeaderView()
     }
     
     private func configTableView() {
-        models.append(Sections(options: [
-            SettingOptions(icon: Images.timeScheduled, title: "إشعارات", handler: {
-                // Go to the Schedulers Notifications
-                let schedulersVC = SchedulersListVC()
-                return self.navigationController?.pushViewController(schedulersVC, animated: true)
-                
-            }),
-            SettingOptions(icon: Images.bookInfo, title: "معلومات الكتاب", handler: {
-                let aboutVC = AboutViewController()
-                aboutVC.modalTransitionStyle = .crossDissolve
-                aboutVC.modalPresentationStyle = .pageSheet
-                self.present(aboutVC, animated: true)
-            })
-        ]))
+        view.addSubview(tableView)
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+        
+        var snapshot = Snapshot()
+        snapshot.appendSections([.main])
+        snapshot.appendItems([.notification, .about])
+        dataSource.applyOnMainThread(snapshot, animatingDifferences: true)
     }
     
+    
+    
+    
+    
+}
+
+extension SettingsViewController {
+    typealias DataSource = UITableViewDiffableDataSource<Section, Row>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Row>
+    
+    
+    enum Section: CaseIterable {
+        case main
+    }
+    
+    enum Row: Hashable {
+        case notification
+        case about
+        
+        static func == (lhs: Row, rhs: Row) -> Bool {
+            return lhs.hashValue == rhs.hashValue
+        }
+        
+        func hash(into hasher: inout Hasher) {
+            switch self {
+            case .notification:
+                hasher.combine("notification")
+            case .about:
+                hasher.combine("about")
+            }
+        }
+    }
+    //    struct SettingOptions: Hashable {
+    //        let icon: UIImage?
+    //        let title: String
+    //        let handler: () -> Void?
+    //    }
+}
+
+extension SettingsViewController: UITableViewDelegate {
     private func configTableHeaderView() {
         let headerView = SettingTableHeaderView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 250))
         tableView.tableHeaderView = headerView
     }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        models.count
-    }
+    //    func numberOfSections(in tableView: UITableView) -> Int {
+    //        models.count
+    //    }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        models[section].options.count
-    }
+    //    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //        models[section].options.count
+    //    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let model = models[indexPath.section].options[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.identifier, for: indexPath) as! SettingsTableViewCell
-        cell.displayData(title: model.title, Image: model.icon)
-        cell.selectionStyle = .none
-        
-        return cell
-    }
+    //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //        let model = models[indexPath.section].options[indexPath.row]
+    //        let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.identifier, for: indexPath) as! SettingsTableViewCell
+    //        cell.displayData(title: model.title, Image: model.icon)
+    //        cell.selectionStyle = .none
+    //
+    //        return cell
+    //    }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let model = models[indexPath.section].options[indexPath.row]
-        model.handler()
+        guard let row = dataSource.itemIdentifier(for: indexPath) else { return }
+        
+        switch row {
+        case .notification:
+            let schedulersVC = SchedulersListVC()
+            let nv = UINavigationController(rootViewController: schedulersVC)
+            self.present(nv, animated: true)
+        case .about:
+            let aboutVC = AboutViewController()
+            aboutVC.modalTransitionStyle = .crossDissolve
+            aboutVC.modalPresentationStyle = .pageSheet
+            self.present(aboutVC, animated: true)
+        }
+            
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         45
     }
-    
 }

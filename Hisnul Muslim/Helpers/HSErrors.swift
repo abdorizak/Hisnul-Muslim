b1c280cd5705b54333a86bcb547c8b624fc9b220
abdorizak <cabdirizaaqyare12@gmail.com>
2024-07-07 10:28:47 +0300

    chore: Update HSErrors.swift with additional error cases

--- Hisnul Muslim/Helpers/HSErrors.swift
+++ Hisnul Muslim/Helpers/HSErrors.swift
@@ -10,5 +10,7 @@ import Foundation
 enum HSErrors: String, Error {
     case alreadyInFavorite = "موجود بالفعل في مفضلة الأدعية..."
     case unableToFavorite = "غير قادر على إضافة هذا إلى المفضلة، لأنه موجود بالفعل في المفضلة"
-    case invalidContext = "invalid Context...."
+    case invalidContext = "سياق غير صالح...."
+    case unableToReadFile = "غير قادر على قراءة ملف..."
+    case fileNotFound = "لم يتم العثور على الملف..."
 }

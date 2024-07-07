d5323c8f3b133619204fc406439b63344a3565e0
abdorizak <cabdirizaaqyare12@gmail.com>
2024-07-07 10:45:52 +0300

    refactor: Update SchedulersListCell to improve layout and remove unused code

--- Hisnul Muslim/Views/Custom Views/Cell/SchedulersListCell.swift
+++ Hisnul Muslim/Views/Custom Views/Cell/SchedulersListCell.swift
@@ -7,50 +7,19 @@
 
 import UIKit
 
-//class SchedulersListCell: UITableViewCell {
-//
-//    static let identifier = String(describing: SchedulersListCell.self)
-//    private var adkar_title = Label(textAlignment: .right, 30)
-//
-//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-//        super.init(style: style, reuseIdentifier: reuseIdentifier)
-//        cellConfig()
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-//    override func layoutSubviews() {
-//        super.layoutSubviews()
-//
-//    }
-//
-//    private func cellConfig() {
-//        contentView.addSubview(adkar_title)
-//        adkar_title.numberOfLines = 0
-//
-//        NSLayoutConstraint.activate([
-//            adkar_title.topAnchor.constraint(equalToSystemSpacingBelow: contentView.layoutMarginsGuide.topAnchor, multiplier: 1),
-//            adkar_title.trailingAnchor.constraint(equalToSystemSpacingAfter: contentView.layoutMarginsGuide.trailingAnchor, multiplier: 1),
-//            adkar_title.leadingAnchor.constraint(equalToSystemSpacingAfter: contentView.layoutMarginsGuide.leadingAnchor, multiplier: 1),
-//            adkar_title.bottomAnchor.constraint(equalToSystemSpacingBelow: contentView.layoutMarginsGuide.bottomAnchor, multiplier: 1)
-//        ])
-//    }
-//
-//    func displayData(list of: HSMSchedulers) {
-//        adkar_title.text = of.adkarName
-//    }
-//
-//}
-
 class SchedulersListCell: UITableViewCell {
     
-    static let identifier = String(describing: SchedulersListCell.self)
     private var adkar_title = Label(textAlignment: .right, 30)
     private var timeView = UIView()
     private var hourLabel = Label(textAlignment: .center, 16)
     private var minuteLabel = Label(textAlignment: .center, 16)
+    private let separatorLabel = Label(textAlignment: .center, 16)
+    private let timeStackView = {
+        $0.axis = .horizontal
+        $0.spacing = 8 // Adjust the spacing as needed
+        $0.alignment = .center
+        return $0.withoutAutoresizingMask
+    }(UIStackView(frame: .zero))
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -67,26 +36,13 @@ class SchedulersListCell: UITableViewCell {
     }
     
     private func cellConfig() {
-        contentView.addSubview(adkar_title)
+        contentView.addSubViews(adkar_title, timeView)
         adkar_title.numberOfLines = 0
 
-        contentView.addSubview(timeView)
         timeView.translatesAutoresizingMaskIntoConstraints = false
-        timeView.backgroundColor = UIColor.random()
+        timeView.backgroundColor = .tertiarySystemGroupedBackground
         timeView.layer.cornerRadius = 12
-
-        // Initialize the separator label
-        let separatorLabel = UILabel()
-        separatorLabel.text = ":"
-        separatorLabel.font = UIFont.systemFont(ofSize: 16)
-        separatorLabel.textColor = .white
-        separatorLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        let timeStackView = UIStackView(arrangedSubviews: [hourLabel, separatorLabel, minuteLabel])
-        timeStackView.axis = .horizontal
-        timeStackView.spacing = 8 // Adjust the spacing as needed
-        timeStackView.alignment = .center
-        timeStackView.translatesAutoresizingMaskIntoConstraints = false
+        timeStackView.addArrangedSubviews(hourLabel, separatorLabel, minuteLabel)
         timeView.addSubview(timeStackView)
 
         NSLayoutConstraint.activate([
@@ -109,54 +65,9 @@ class SchedulersListCell: UITableViewCell {
     }
 
 
-    
-//    private func cellConfig() {
-//        contentView.addSubview(adkar_title)
-//        adkar_title.numberOfLines = 0
-//        
-//        contentView.addSubview(timeView)
-//        timeView.translatesAutoresizingMaskIntoConstraints = false
-//        timeView.backgroundColor = UIColor.random()
-//        timeView.addSubview(hourLabel)
-//        timeView.addSubview(minuteLabel)
-//        
-//        NSLayoutConstraint.activate([
-//            adkar_title.topAnchor.constraint(equalToSystemSpacingBelow: contentView.layoutMarginsGuide.topAnchor, multiplier: 1),
-//            adkar_title.trailingAnchor.constraint(equalToSystemSpacingAfter: contentView.layoutMarginsGuide.trailingAnchor, multiplier: 1),
-//            adkar_title.leadingAnchor.constraint(equalToSystemSpacingAfter: timeView.layoutMarginsGuide.trailingAnchor, multiplier: 1),
-//            
-//            timeView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-//            timeView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-//            timeView.widthAnchor.constraint(equalToConstant: 100),
-//            timeView.heightAnchor.constraint(equalToConstant: 35),
-//            
-//            hourLabel.centerXAnchor.constraint(equalTo: timeView.centerXAnchor, constant: -10),
-//            hourLabel.centerYAnchor.constraint(equalTo: timeView.centerYAnchor),
-//            
-//            minuteLabel.centerXAnchor.constraint(equalTo: timeView.centerXAnchor, constant: 10),
-//            minuteLabel.centerYAnchor.constraint(equalTo: timeView.centerYAnchor),
-//        ])
-//        
-//        // Add ":" between hour and minute labels
-//        let separatorLabel = UILabel()
-//        separatorLabel.text = ":"
-//        separatorLabel.font = UIFont.systemFont(ofSize: 15)
-//        separatorLabel.textColor = .white
-//        separatorLabel.translatesAutoresizingMaskIntoConstraints = false
-//        timeView.addSubview(separatorLabel)
-//        separatorLabel.centerXAnchor.constraint(equalTo: timeView.centerXAnchor).isActive = true
-//        separatorLabel.centerYAnchor.constraint(equalTo: timeView.centerYAnchor).isActive = true
-//    }
-
     func displayData(list of: HSMSchedulers) {
         adkar_title.text = of.adkarName
         hourLabel.text = of.hour
         minuteLabel.text = of.minute
-    }
-}
-
-extension UIColor {
-    static func random() -> UIColor {
-        return UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
     }
 }

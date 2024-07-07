ead70ff5f05f323f4f4a6938edcc93e4f38db23e
abdorizak <cabdirizaaqyare12@gmail.com>
2024-07-07 10:38:16 +0300

    refactor: Update DetailsViewModel to use Combine for event handling and state management

--- Hisnul Muslim/View Model/DetailsViewModel.swift
+++ Hisnul Muslim/View Model/DetailsViewModel.swift
@@ -6,3 +6,62 @@
 //
 
 import Foundation
+import Combine
+
+class DetailsViewModel {
+    let event = PassthroughSubject<Event, Never>()
+    @Published var state: State
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(content: Content) {
+        self.state = State(details: content)
+        event
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] in self?.handleEvent($0) }
+            .store(in: &cancellables)
+    }
+    
+    private func handleEvent(_ event: Event) {
+        switch event {
+        case .addFavorite(let content):
+            addFavorite(content)
+        case .notify:
+            checkNotificationAccess()
+        default:
+            break
+        }
+    }
+    
+    func addFavorite(_ content: Content) {
+        let favorite = Content(id: content.id, title: content.title, pages: content.pages)
+        
+        Task {
+            do {
+                try await PersistenceManager.updateWith(favorite: favorite, actionType: .add)
+                event.send(.showMessage("نجاح", "تمت الإضافة إلى المفضلة.", .ok))
+            } catch {
+                event.send(.showMessage("خطأ", "فشل في الإضافة إلى المفضلة.", .ok))
+            }
+        }
+    }
+    
+    func checkNotificationAccess() {
+        if SchedulerNotifications.shared.isUserAllowNotification {
+            event.send(.didNotify(state.details))
+        } else {
+            event.send(.showMessage("خطأ", "يجب أن تسمح بالإشعارات في الإعدادات.", .okWithSettings))
+        }
+    }
+}
+
+extension DetailsViewModel {
+    enum Event {
+        case addFavorite(Content)
+        case notify, didNotify(Content)
+        case showMessage(String, String, AlertVC.AlertAction)
+    }
+    
+    struct State {
+        var details: Content
+    }
+}

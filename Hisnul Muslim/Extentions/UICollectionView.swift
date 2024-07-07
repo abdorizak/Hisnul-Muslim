ff8f24ad67a45f99d7c954d884ac78eb942ab497
abdorizak <cabdirizaaqyare12@gmail.com>
2024-07-07 10:23:48 +0300

    chore: Add applyOnMainThread method to UICollectionViewDiffableDataSource extension

--- Hisnul Muslim/Extentions/UICollectionView.swift
+++ Hisnul Muslim/Extentions/UICollectionView.swift
@@ -15,3 +15,11 @@ extension UICollectionView {
     }
     
 }
+
+extension UICollectionViewDiffableDataSource where SectionIdentifierType: Hashable, ItemIdentifierType: Hashable {
+    func applyOnMainThread(_ snapshot: NSDiffableDataSourceSnapshot<SectionIdentifierType, ItemIdentifierType>, animatingDifferences: Bool = true, completion: (() -> Void)? = nil) {
+        DispatchQueue.main.async {
+            self.apply(snapshot, animatingDifferences: animatingDifferences, completion: completion)
+        }
+    }
+}

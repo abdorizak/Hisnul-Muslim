5f8ff12a5e9bc00aefb082c8098c35400b29ecf6
abdorizak <cabdirizaaqyare12@gmail.com>
2024-07-07 10:43:34 +0300

    refactor: Update AlertVC to support multiple actions in the alert view

--- Hisnul Muslim/Views/Custom Views/AlertVC.swift
+++ Hisnul Muslim/Views/Custom Views/AlertVC.swift
@@ -10,42 +10,44 @@ import UIKit
 
 class AlertVC: UIViewController {
     
-    let containerView   = AlertContainerView()
-    let titleLabel      = Label(textAlignment: .center, 20)
-    let messageLabel    = Label(textAlignment: .center)
-    let actionButton    = Button(color: .systemPink, title: "Ok", systemImageName: "checkmark.circle")
+    enum AlertAction {
+        case ok
+        case okWithSettings
+        // Add more cases as needed
+    }
+
+    let containerView = AlertContainerView()
+    let titleLabel = Label(textAlignment: .center, 20)
+    let messageLabel = Label(textAlignment: .center)
     
     var alertTitle: String?
     var message: String?
-    var buttonTitle: String?
+    var actions: [AlertAction]?
     
     let padding: CGFloat = 20
     
-    
-    init(title: String, message: String, buttonTitle: String) {
+    init(title: String, message: String, actions: [AlertAction]) {
         super.init(nibName: nil, bundle: nil)
-        self.alertTitle     = title
-        self.message        = message
-        self.buttonTitle    = buttonTitle
+        self.alertTitle = title
+        self.message = message
+        self.actions = actions
     }
     
-    
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
-        view.addSubViews(containerView, titleLabel, actionButton, messageLabel)
+        view.addSubViews(containerView, titleLabel, messageLabel)
         
         configureContainerView()
         configureTitleLabel()
-        configureActionButton()
         configureMessageLabel()
+        configureButtons()
     }
-    
     
     func configureContainerView() {
         NSLayoutConstraint.activate([
@@ -55,7 +57,6 @@ class AlertVC: UIViewController {
             containerView.heightAnchor.constraint(equalToConstant: 220)
         ])
     }
-    
     
     func configureTitleLabel() {
         titleLabel.text = alertTitle ?? "Something went wrong"
@@ -68,34 +69,74 @@ class AlertVC: UIViewController {
         ])
     }
     
-    
-    func configureActionButton() {
-        actionButton.setTitle(buttonTitle ?? "Ok", for: .normal)
-        actionButton.addTarget(self, action: #selector(dismissVC), for: .touchUpInside)
-        
-        NSLayoutConstraint.activate([
-            actionButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -padding),
-            actionButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
-            actionButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
-            actionButton.heightAnchor.constraint(equalToConstant: 44)
-        ])
-    }
-    
-    
     func configureMessageLabel() {
-        messageLabel.text           = message ?? "Unable to complete request"
-        messageLabel.numberOfLines  = 4
+        messageLabel.text = message ?? "Unable to complete request"
+        messageLabel.numberOfLines = 4
         
         NSLayoutConstraint.activate([
             messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
             messageLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
             messageLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
-            messageLabel.bottomAnchor.constraint(equalTo: actionButton.topAnchor, constant: -12)
+            messageLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -64)
         ])
     }
     
+    func configureButtons() {
+        guard let actions = actions else { return }
+        
+        let buttonStackView = UIStackView()
+        buttonStackView.axis = .horizontal
+        buttonStackView.distribution = .fillEqually
+        buttonStackView.spacing = 12
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        for action in actions {
+            let button = UIButton(type: .system)
+            button.setTitleColor(.white, for: .normal)
+            button.layer.cornerRadius = 10
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+            
+            switch action {
+            case .ok:
+                button.setTitle("OK", for: .normal)
+                button.backgroundColor = .systemPink
+                button.addTarget(self, action: #selector(dismissVC), for: .touchUpInside)
+                
+            case .okWithSettings:
+                let settingsButton = UIButton(type: .system)
+                settingsButton.setTitle("Settings", for: .normal)
+                settingsButton.backgroundColor = .systemBlue
+                settingsButton.setTitleColor(.white, for: .normal)
+                settingsButton.layer.cornerRadius = 10
+                settingsButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+                settingsButton.addTarget(self, action: #selector(openSettings), for: .touchUpInside)
+                buttonStackView.addArrangedSubview(settingsButton)
+                
+                button.setTitle("OK", for: .normal)
+                button.backgroundColor = .systemPink
+                button.addTarget(self, action: #selector(dismissVC), for: .touchUpInside)
+            }
+            
+            buttonStackView.addArrangedSubview(button)
+        }
+        
+        view.addSubview(buttonStackView)
+        
+        NSLayoutConstraint.activate([
+            buttonStackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
+            buttonStackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+            buttonStackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -padding),
+            buttonStackView.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
     
     @objc func dismissVC() {
         dismiss(animated: true)
+    }
+    
+    @objc func openSettings() {
+        if let url = URL(string: UIApplication.openSettingsURLString) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
     }
 }

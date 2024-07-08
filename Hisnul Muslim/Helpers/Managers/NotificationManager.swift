fd3172cb1d421454290121c532ef097bfcde7311
abdorizak <cabdirizaaqyare12@gmail.com>
2024-07-08 10:19:34 +0300

    refactor: Update SchedulerNotifications to use async/await for notification authorization and scheduling

--- Hisnul Muslim/Helpers/Managers/NotificationManager.swift
+++ Hisnul Muslim/Helpers/Managers/NotificationManager.swift
@@ -13,41 +13,37 @@ final class SchedulerNotifications {
     static let shared = SchedulerNotifications()
     
     var isUserAllowNotification: Bool {
-        var isAuthorized = false
-        let semaphore = DispatchSemaphore(value: 0)
-        UNUserNotificationCenter.current().getNotificationSettings { settings in
-            isAuthorized = settings.authorizationStatus == .authorized
-            semaphore.signal()
+        get async {
+            let settings = await UNUserNotificationCenter.current().notificationSettings()
+            return settings.authorizationStatus == .authorized
         }
-        semaphore.wait()
-        return isAuthorized
     }
     
-    func scheduleDailyNotification(identifier: String, hour: Int, minute: Int, AdkarContent: String, completion: @escaping (String?) -> Void) {
+    func scheduleDailyNotification(identifier: String, hour: Int, minute: Int, AdkarContent: String) async throws -> String {
         guard let uuid = UUID(uuidString: identifier) else {
-            completion("Invalid identifier")
-            return
+            throw NSError(domain: "SchedulerNotifications", code: 1, userInfo: [NSLocalizedDescriptionKey: "Invalid identifier"])
         }
+        
         let content = UNMutableNotificationContent()
         content.title = AdkarContent
         content.body = "لقد طلبت منا تذكيرهذه الأذكار:\(AdkarContent)"
         content.sound = UNNotificationSound.default
         
         var dateComponents = DateComponents()
-        dateComponents.hour = Int(hour)
-        dateComponents.minute = Int(minute)
+        dateComponents.hour = hour
+        dateComponents.minute = minute
         
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
         let request = UNNotificationRequest(identifier: uuid.uuidString, content: content, trigger: trigger)
         
-        UNUserNotificationCenter.current().add(request) { error in
-            if let error = error {
-                completion("Error scheduling notification: \(error.localizedDescription)")
-            } else {
-                completion("لقد طلبت إرسال إشعار بخصوص هذا الإعلان:\(AdkarContent) ، سنرسل لك الشكر")
+        return try await withCheckedThrowingContinuation { continuation in
+            UNUserNotificationCenter.current().add(request) { error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume(returning: "لقد طلبت إرسال إشعار بخصوص هذا الإعلان:\(AdkarContent) ، سنرسل لك الشكر")
+                }
             }
         }
     }
-
-
 }
